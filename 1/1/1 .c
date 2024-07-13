d9aeef8bbff87c292c27f96a0b23728c44472bfd
Qqqi7711 <3461418015@qq.com>
2024-07-13 15:01:21 +0800

    Add files via upload

---
+++ 1/1/1 .c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//int main()
+//{
+//	printf("张曼是个超级大傻逼\n");
+//	return 0;
+//}
+
+
+
+int main()
+{
+	printf("  3333 \n");
+		return 0;
+}
---
+++ 1/1/11.c
@@ -0,0 +1,6 @@
+ #include<stdio.h>
+int main()
+{
+	printf("ÎâÃÏ¾û\n");
+	return 0;
+}
